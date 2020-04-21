b40cb2a21745c170c05e1bf63178259575d8729b
Kevin <Aok4001032@gmail.com>
2020-04-21 16:28:14 -0400

    agregue_plantilla

--- datos/habitacion.php
+++ datos/habitacion.php
@@ -24,6 +24,19 @@
          }
      }
 
+    
+    public function crear(){
+      
+    }
+
+     public function create(){
+      try {
+          $stm = $this->pdo->prepare("INSERT INTO ".self::TABLE." (name,lastName,sex,address,phone,age) VALUES (?,?,?,?,?,?)");
+      $stm->execute(array($this->name,$this->lastName,$this->sex,$this->address,$this->phone,$this->age));
+      } catch (PDOException $e) {
+          echo $e->getMessage();
+      }
+  }
 
 
     }
